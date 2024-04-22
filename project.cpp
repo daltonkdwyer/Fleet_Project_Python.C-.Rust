0c01db7b536728251a062d1d865cdc4564b359a7
daltonkdwyer <dalton.dwyer@gmail.com>
2024-04-22 17:21:44 -0400

    a

--- project.cpp
+++ project.cpp
@@ -3,31 +3,45 @@
 
 using namespace std;
 
-class Vehilce{
+class Vehicle{
     public:
         string name;
 
-}
+};
 
 class Fleet{
     public:
         void print_vehicles();
-        void add_vehicles(Vehicle vehicle)
+        void add_vehicles(Vehicle new_vehicle);
     
     private:
         vector<Vehicle> vehicle_list;
+};
+
+void Fleet::add_vehicles(Vehicle new_vehicle){
+    this->vehicle_list.push_back(new_vehicle);
 }
 
-Fleet::print_vehicles(){
-    for (Vehicle v : vehicle_list){
-        cout << v.name << endl;
+void Fleet::print_vehicles(){
+    for (int i=0; i < vehicle_list.size(); i++ ){
+        cout << vehicle_list[i].name << endl;
     }
 }
 
 
 int main(){
-    Vehicle myVehicle = new Vehicle
+    Vehicle myVehicle1;
+    myVehicle1.name = "Drone1";
+
+    Vehicle myVehicle2;
+    myVehicle2.name = "Drone2";
+
+    Vehicle myVehicle3;
+    myVehicle3.name = "Drone3";
+
     Fleet myFleet;
-    Fleet.add_vehicles(myVehicle)
-    Fleet.print_vehicles()
+    myFleet.add_vehicles(myVehicle1);
+    myFleet.add_vehicles(myVehicle2);
+    myFleet.add_vehicles(myVehicle3);
+    myFleet.print_vehicles();
 }
