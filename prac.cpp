3638ed5b386d6e482e91643df44c9b00348f5e95
daltonkdwyer <dalton.dwyer@gmail.com>
2024-05-15 11:21:42 -0400

    a

--- prac.cpp
+++ prac.cpp
@@ -6,21 +6,26 @@ string my_name = "Dalton";
 string* my_name_pointer = &my_name;
 string my_var1 = my_name;
 
+string& reference_name = my_name;
+
+
+
 // string my_var1 = *my_name_pointer;
 // string& my_var2 = *my_name_pointer;
 
-void change_name_raw(string name){
-    name += "T";
-    cout << name << endl;
-}
+// void change_name_raw(string& name){
+//     name += "T";
+//     cout << name << endl;
+// }
 
 int main(){
+    reference_name += "I";
+    cout << reference_name << endl;
     // cout << &my_name << endl;
     // cout << &my_var1 << endl;
     // cout << &my_var2 << endl;
-    change_name_raw(my_name);
-    cout << my_name << endl;
-
+    // change_name_raw(my_name);
+    // cout << my_name << endl;
 }
 
 
